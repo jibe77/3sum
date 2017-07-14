65e911e4aa1d8dadcbc9ed6b10e6cfd4775534f3
jean-baptiste renaux <jb.renaux@me.com>
2017-07-14 23:56:11 +0200

    Add unit-tests on REST controller.

--- src/test/java/org/jibe77/sum/test/MainRestTest.java
+++ src/test/java/org/jibe77/sum/test/MainRestTest.java
@@ -1,8 +1,6 @@
 package org.jibe77.sum.test;
 
 import org.jibe77.sum.Main;
-import org.jibe77.sum.Result;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,13 +15,8 @@ import org.springframework.web.context.WebApplicationContext;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -68,39 +61,32 @@ public class MainRestTest {
         this.mvc.perform(
                 get("/sum/4,5,1,-2,-7,2,-5,-3,-7,-3,1"))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("[{\"i\":-7,\"j\":2,\"k\":5},{\"i\":-5,\"j\":1,\"k\":4},{\"i\":-3,\"j\":-2,\"k\":5},{\"i\":-3,\"j\":1,\"k\":2},{\"i\":-2,\"j\":1,\"k\":1}]")));
-    }
-/*
-    @Test
-    public void mainWithResult2Test() {
-        // set up
-        Integer[] array = new Integer[] {-1, -6, -3, -7, 5, -8, 2, -8, 1};
-
-        // launch
-        Main m = new Main();
-        List<Result> returnValue = m.home(array);
-
-        // check
-        assertThat(returnValue).isNotNull();
-        assertThat(returnValue.size()).isEqualTo(3);
-        assertThat(returnValue.get(0).toString()).isEqualTo("-7 2 5");
-        assertThat(returnValue.get(1).toString()).isEqualTo("-6 1 5");
-        assertThat(returnValue.get(2).toString()).isEqualTo("-3 1 2");
+                .andExpect(content().string(equalTo(
+                        "[{\"i\":-7,\"j\":2,\"k\":5}," +
+                                "{\"i\":-5,\"j\":1,\"k\":4}," +
+                                "{\"i\":-3,\"j\":-2,\"k\":5}," +
+                                "{\"i\":-3,\"j\":1,\"k\":2}," +
+                                "{\"i\":-2,\"j\":1,\"k\":1}]")));
     }
 
     @Test
-    public void mainWithResult3Test() {
-        // set up
-        Integer[] array = new Integer[] {-5, -1, -4, 2, 9, -9, -6, -1, -7};
+    public void mainWithResult2Test() throws Exception {
+        this.mvc.perform(
+                get("/sum/-1,-6,-3,-7,5,-8,2,-8,1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo(
+                        "[{\"i\":-7,\"j\":2,\"k\":5}," +
+                                "{\"i\":-6,\"j\":1,\"k\":5}," +
+                                "{\"i\":-3,\"j\":1,\"k\":2}]")));
+    }
 
-        // launch
-        Main m = new Main();
-        List<Result> returnValue = m.home(array);
-
-        // check
-        assertThat(returnValue).isNotNull();
-        assertThat(returnValue.size()).isEqualTo(2);
-        assertThat(returnValue.get(0).toString()).isEqualTo("-5 -4 9");
-        assertThat(returnValue.get(1).toString()).isEqualTo("-1 -1 2");
-    }*/
+    @Test
+    public void mainWithResult3Test() throws Exception {
+        this.mvc.perform(
+                get("/sum/-5,-1,-4,2,9,-9,-6,-1,-7"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo(
+                        "[{\"i\":-5,\"j\":-4,\"k\":9}," +
+                                "{\"i\":-1,\"j\":-1,\"k\":2}]")));
+    }
 }
